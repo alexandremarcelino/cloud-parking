12e6e6dc3db4a97ccf78acb8672df5cf364fb2bb
Alexandre Marcelino <alexandre.hoffmann.marcelino@gmail.com>
2023-10-24 19:18:46 -0300

    Primeiro endpoint de lista

--- src/main/java/br/com/marcelino/parking/controller/HelloController.java
+++ src/main/java/br/com/marcelino/parking/controller/HelloController.java
@@ -3,9 +3,11 @@ package br.com.marcelino.parking.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import springfox.documentation.annotations.ApiIgnore;
 
 @Controller
 @RequestMapping("/")
+@ApiIgnore
 public class HelloController {
 
     @GetMapping
--- src/main/java/br/com/marcelino/parking/controller/ParkingController.java
+++ src/main/java/br/com/marcelino/parking/controller/ParkingController.java
@@ -4,11 +4,14 @@ import br.com.marcelino.parking.controller.dto.ParkingDTO;
 import br.com.marcelino.parking.controller.mapper.ParkingMapper;
 import br.com.marcelino.parking.model.Parking;
 import br.com.marcelino.parking.service.ParkingService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RestController
 @RequestMapping("/parking")
 public class ParkingController {
 
@@ -21,9 +24,9 @@ public class ParkingController {
     }
 
     @GetMapping
-    public List<ParkingDTO> findAll() {
+    public ResponseEntity<List<ParkingDTO>> findAll() {
         List<Parking> parkingList = parkingService.findAll();
         List<ParkingDTO> result = parkingMapper.toParkingDTOList(parkingList);
-        return result;
+        return ResponseEntity.ok(result);
     }
 }
